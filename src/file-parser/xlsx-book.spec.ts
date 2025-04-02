d0c10b380fcc25fb4bf39752af0b49d7f65ca923
gen <kyliki@yandex.ru>
2025-04-03 01:36:54 +0300

    change jest to vitest

--- src/file-parser/xlsx-book.spec.ts
+++ src/file-parser/xlsx-book.spec.ts
@@ -1,37 +1,20 @@
-import * as XLSX from 'xlsx';
-
-import fs from 'fs';
+import * as fs from 'fs';
 import { XlsxBook } from './xlsx-book.js';
+import { describe, it, vi } from 'vitest';
 
-// Мокаем fs и xlsx для изоляции
-// jest.mock('fs');
-jest.mock('xlsx');
-jest.mock('fs');
+vi.mock('fs', async (importOriginal) => {
+  const actualFs = await importOriginal<typeof fs>();
+  return {
+    ...actualFs,
+    readFileSync: vi.fn(() => Buffer.from('mock excel data')),
+  };
+});
 
 describe('xlsxBook', () => {
   const mockPublicDir = '/mock/public';
   const mockFileName = 'test.xlsx';
-  const mockFilePath = `${mockPublicDir}/test.xlsx`;
-
-  beforeEach(() => {
-    jest.clearAllMocks();
-  });
 
   it('должен создавать workbook из файла', () => {
-    const mockBinaryData = 'mockBinaryData';
-    const mockArrayBuffer = new Uint8Array([1, 2, 3]);
-    const mockWorkBook = { Sheets: { Sheet1: {} } };
-
-    (fs.readFileSync as jest.Mock).mockReturnValue(mockBinaryData);
-    (XLSX.read as jest.Mock).mockReturnValue(mockWorkBook);
-
-    const book = new XlsxBook(mockPublicDir, mockFileName);
-    expect(fs.readFileSync).toHaveBeenCalledWith(mockFilePath, {
-      encoding: 'binary',
-    });
-    expect(XLSX.read).toHaveBeenCalledWith(expect.any(Uint8Array), {
-      type: 'array',
-    });
-    expect(book.getWorkBook()).toBe(mockWorkBook);
+    const xlsxBook = new XlsxBook(mockPublicDir, mockFileName);
   });
 });
