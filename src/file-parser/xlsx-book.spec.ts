21f58610e8d5edd5879bce1089e85d7c00d3264e
gen <kyliki@yandex.ru>
2025-04-03 22:51:24 +0300

    tests for XlsxBook

--- src/file-parser/xlsx-book.spec.ts
+++ src/file-parser/xlsx-book.spec.ts
@@ -1,20 +1,63 @@
 import * as fs from 'fs';
 import { XlsxBook } from './xlsx-book.js';
-import { describe, it, vi } from 'vitest';
+import { beforeEach, describe, expect, it, vi } from 'vitest';
+import * as XLSX from 'xlsx';
 
-vi.mock('fs', async (importOriginal) => {
-  const actualFs = await importOriginal<typeof fs>();
-  return {
-    ...actualFs,
-    readFileSync: vi.fn(() => Buffer.from('mock excel data')),
-  };
-});
+const fakeWorkbook = { Sheets: {}, SheetNames: ['Sheet1'] };
+
+vi.mock('fs', () => ({ readFileSync: vi.fn() }));
+vi.mock('xlsx', () => ({
+  read: vi.fn(() => fakeWorkbook),
+}));
 
 describe('xlsxBook', () => {
   const mockPublicDir = '/mock/public';
   const mockFileName = 'test.xlsx';
+  const mockFilePath = `${mockPublicDir}/${mockFileName}`;
 
-  it('должен создавать workbook из файла', () => {
-    const xlsxBook = new XlsxBook(mockPublicDir, mockFileName);
+  beforeEach(() => {
+    vi.resetAllMocks();
+  });
+
+  describe('createPath', () => {
+    it('должен возвращать корректный путь', () => {
+      const xlsxBook = new XlsxBook(mockPublicDir, mockFileName);
+      const receivedFilePath = xlsxBook['createPath']();
+      expect(receivedFilePath).toBe(mockFilePath);
+    });
+  });
+
+  describe('initializeWorkBook', () => {
+    it('должен корректно загружать workbook', () => {
+      const fakeBinaryData = Buffer.from('fake excel data');
+      vi.spyOn(fs, 'readFileSync').mockReturnValue(fakeBinaryData);
+
+      new XlsxBook(mockPublicDir, mockFileName);
+
+      expect(fs.readFileSync).toHaveBeenCalledWith(mockFilePath);
+      expect(XLSX.read).toHaveBeenCalledWith(new Uint8Array(fakeBinaryData), {
+        type: 'array',
+      });
+    });
+
+    it('должен выбрасывать ошибку, если файл не найден', () => {
+      (
+        fs.readFileSync as unknown as ReturnType<typeof vi.fn>
+      ).mockImplementation(() => {
+        throw new Error('File not found');
+      });
+
+      expect(() => new XlsxBook(mockPublicDir, mockFileName)).toThrow(
+        `Ошибка при чтении файла ${mockFilePath}: File not found`
+      );
+    });
+  });
+
+  describe('getWorkBook', () => {
+    it('должен вернуть workbook', () => {
+      const xlsxBook = new XlsxBook(mockPublicDir, mockFileName);
+      const receivedBook = xlsxBook.getWorkBook();
+      expect(receivedBook).toBe(fakeWorkbook);
+    });
   });
 });
