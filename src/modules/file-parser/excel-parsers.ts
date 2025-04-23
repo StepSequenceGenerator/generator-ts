ea3b2da38961ef2ca9a1d8dde586fcc65666acc3
gen <kyliki@yandex.ru>
2025-04-23 23:12:23 +0300

    создал класс DefaultExcelParser

--- src/modules/file-parser/excel-parsers.ts
+++ src/modules/file-parser/excel-parsers.ts
@@ -1,6 +1,8 @@
 import { BaseExcelParser } from './BaseExcelParser.js';
 import { ColumnName } from '../../enums/column-name-enum.js';
 
-const defaultExcelParser = new BaseExcelParser<typeof ColumnName>(ColumnName);
+export class DefaultExcelParser extends BaseExcelParser<typeof ColumnName> {}
+
+const defaultExcelParser = new DefaultExcelParser(ColumnName);
 
 export { defaultExcelParser };
