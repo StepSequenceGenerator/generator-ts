44065a474258d4f52dcc197ce56b8c05482d2c4e
gen <kyliki@yandex.ru>
2025-05-11 22:11:13 +0300

    exporting utils

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,14 @@
 import { twizzle } from './twizzle.js';
-import { DifficultLevelAmountStep } from './shared/enums/difficult-level-amount-step-enum.js';
-import { createDistanceFactor } from './shared/types/distance-factor.type';
+import { utils } from './utils/exporting-utils';
 
 export default twizzle;
-export { DifficultLevelAmountStep };
+export { utils };
 
 if (process.env.NODE_ENV === 'development') {
   const tw = twizzle();
   tw.init();
-  const distanceFactor = createDistanceFactor(3);
-  const seq = tw.generateSequence(DifficultLevelAmountStep.LEVEL_4, distanceFactor);
+  const distanceFactor = utils.createDistanceFactor(3);
+  const seq = tw.generateSequence(utils.DifficultLevelAmountStep.LEVEL_4, distanceFactor);
   console.log(
     seq.map((item) => {
       return `distance: ${item.distance * distanceFactor} \n ${item.id}:  ${item.name} | ${item.startLeg} - ${item.endLeg} | x: ${item.coordinates.end.x} y: ${item.coordinates.end.x}`;
---
+++ src/utils/exporting-utils.ts
@@ -0,0 +1,7 @@
+import { createDistanceFactor } from '../shared/types/distance-factor.type';
+import { DifficultLevelAmountStep } from '../shared/enums/difficult-level-amount-step-enum';
+
+export const utils = {
+  createDistanceFactor,
+  DifficultLevelAmountStep,
+};
